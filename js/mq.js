5a43656915e8bf6809d8f5560d44a8b070fde617
cohennamit <cohennamitt@gmail.com>
2025-04-02 21:43:12 +0300

    Resolved merge conflict in mq.js

--- js/mq.js
+++ js/mq.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-/* © 2024 InvestingHelper by Or Malka. All rights reserved. | © 2024 InvestingHelper by Or Malka. כל הזכויות שמורות. */
+/* © 2024 InvestingHelper by Or Malka. All rights reserved. | © 2024 InvestingHelper by Or Malka. כל הזכויות שמורות. */
 /*!
  * Copyright (c) 2024 InvestingHelper by Or Malka. All rights reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
@@ -8,7 +7,7 @@
 /*!
  * זכויות יוצרים (c) 2024 InvestingHelper מאת אור מלכה. כל הזכויות שמורות.
  * העתקה בלתי מורשית של קובץ זה, בכל אמצעי, אסורה בהחלט.
- * קנייני וסודי.
+ * קנייני וסודי.
  */
 
 function setResponsiveImages() {
@@ -18,14 +17,12 @@ function setResponsiveImages() {
             img.src = img.getAttribute('data-desktop')
         } else {
             img.src = img.getAttribute('data-mobile')
-
         }
     })
 }
 
 setResponsiveImages()
 window.addEventListener('resize', setResponsiveImages)
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const images = document.querySelectorAll('.scroll-img');
@@ -48,55 +45,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', checkScroll);
     checkScroll();
-=======
-/* © 2024 InvestingHelper by Or Malka. All rights reserved. | © 2024 InvestingHelper by Or Malka. כל הזכויות שמורות. */
-/*!
- * Copyright (c) 2024 InvestingHelper by Or Malka. All rights reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-/*!
- * זכויות יוצרים (c) 2024 InvestingHelper מאת אור מלכה. כל הזכויות שמורות.
- * העתקה בלתי מורשית של קובץ זה, בכל אמצעי, אסורה בהחלט.
- * קנייני וסודי.
- */
-
-function setResponsiveImages() {
-    const IMAGES = document.querySelectorAll('.responsive-img')
-    IMAGES.forEach((img) => {
-        if (window.innerWidth >= 768) {
-            img.src = img.getAttribute('data-desktop')
-        } else {
-            img.src = img.getAttribute('data-mobile')
-
-        }
-    })
-}
-
-setResponsiveImages()
-window.addEventListener('resize', setResponsiveImages)
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('.scroll-img');
-
-    function checkScroll() {
-        const triggerBottom = window.innerHeight / 6 * 4;
-        const triggerTop = window.innerHeight / 3;
-
-        images.forEach(img => {
-            const imageTop = img.getBoundingClientRect().top;
-            const imageBottom = img.getBoundingClientRect().bottom;
-
-            if (imageTop < triggerBottom && imageBottom > triggerTop) {
-                img.classList.add('visible');
-                // } else {
-                //     img.classList.remove('visible');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkScroll);
-    checkScroll();
->>>>>>> dfd67d973d3278937db1f4daea4227a32c1fe1e1
 });
